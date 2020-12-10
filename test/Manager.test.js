d30283e240c6e306b4c395f1562273bb98a8596f
James Gould <nobleburgundy@gmail.com>
2020-12-09 22:43:23 -0600

    Added parameter checking and tests to Manager class.

--- test/Manager.test.js
+++ test/Manager.test.js
@@ -2,19 +2,35 @@ const Manager = require("../lib/Manager");
 const Employee = require("../lib/Employee");
 
 test("Can set office number via constructor argument", () => {
-  const testValue = 100;
+  const testValue = "100";
   const e = new Manager("Foo", 1, "test@test.com", testValue);
   expect(e.officeNumber).toBe(testValue);
 });
 
+test("Should throw error if 'officeNumber' passed as non-string", () => {
+  const officeNumber = 123;
+  const cb = () => new Manager("Test", 1, "test@test.com", officeNumber);
+  const error = new Error("Expected parameter 'officeNumber' to be a non-empty string.");
+
+  expect(cb).toThrowError(error);
+});
+
+test("Should throw error if 'officeNumber' passed as an empty string", () => {
+  const officeNumber = "";
+  const cb = () => new Manager("Test", 1, "test@test.com", officeNumber);
+  const error = new Error("Expected parameter 'officeNumber' to be a non-empty string.");
+
+  expect(cb).toThrowError(error);
+});
+
 test('getRole() should return "Manager"', () => {
   const testValue = "Manager";
-  const e = new Manager("Foo", 1, "test@test.com", 100);
+  const e = new Manager("Foo", 1, "test@test.com", "100");
   expect(e.getRole()).toBe(testValue);
 });
 
 test("Can get office number via getOffice()", () => {
-  const testValue = 100;
+  const testValue = "100";
   const e = new Manager("Foo", 1, "test@test.com", testValue);
   expect(e.getOfficeNumber()).toBe(testValue);
 });
